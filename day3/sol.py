1e12873bb2aef728c52fe0ecb37768e5608e900c
Q-Mart <qumarthjash@gmail.com>
2015-12-08 12:45:54 +0000

    day 3 part 2

--- day3/sol.py
+++ day3/sol.py
@@ -1,20 +1,42 @@
-no_visited_houses = 1
+no_visited_houses = 0
 
 def mapFrequency(input):
     visited_houses = {}
     x = 0
     y = 0
 
-    for char in input:
-        if char == '^': y += 1
-        if char == 'v': y -= 1
-        if char == '>': x += 1
-        if char == '<': x -= 1
+    #Robo santa
+    x1 = 0
+    y1 = 0
 
-        try:
-            visited_houses[(x,y)] += 1
-        except KeyError:
-            visited_houses[(x,y)] = 0
+    for i in xrange(len(input)):
+
+        isOddIteration = i%2
+        char = input[i]
+
+        if isOddIteration:
+
+            if char == '^': y += 1
+            if char == 'v': y -= 1
+            if char == '>': x += 1
+            if char == '<': x -= 1
+
+            try:
+                visited_houses[(x,y)] += 1
+            except KeyError:
+                visited_houses[(x,y)] = 0
+
+        else:
+
+            if char == '^': y1 += 1
+            if char == 'v': y1 -= 1
+            if char == '>': x1 += 1
+            if char == '<': x1 -= 1
+
+            try:
+                visited_houses[(x1,y1)] += 1
+            except KeyError:
+                visited_houses[(x1,y1)] = 0
 
     return visited_houses
 
