12914a565bfd6109e95929e0fdaa89553ebe04cb
Q-Mart <qumarthjash@gmail.com>
2016-12-04 09:27:04 +0000

    Day 2 part 1

--- 2016/src/day2.cpp
+++ 2016/src/day2.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
 #include <string>
-#include <string>
+#include <fstream>
 
+int x = 1;
+int y = 1;
+std::string keycode = "";
+const int KEYPAD[3][3] = { {1,2,3},
+                           {4,5,6},
+                           {7,8,9} };
 
-class keypadTraverser {
-  int x;
-  int y;
-  std::string keycode;
-  const static int KEYPAD[3][3];
+void processInstruction(char);
+std::string processInstructionSet(char*);
 
-  keypadTraverser();
-  void processInstruction(char);
-  std::string processInstructionSet(char*);
-};
-
-keypadTraverser::keypadTraverser() {
-  x = 1;
-  y = 1;
-  keycode = "";
-  const static int KEYPAD[3][3] = { {1,2,3},
-                                    {4,5,6},
-                                    {7,8,9} };
-}
-
-void keypadTraverser::processInstruction(char c) {
+void processInstruction(char c) {
   switch (c) {
     case 'U' : --y;
                break;
@@ -33,26 +22,37 @@ void keypadTraverser::processInstruction(char c) {
                break;
     case 'R' : ++x;
                break;
-    case '\n' : keycode += KEYPAD[y][x];
+    case '\n' : keycode += std::to_string(KEYPAD[y][x]);
                 break;
+    default: break;
   }
 
   int* vars[] = {&x,&y};
   for (auto i : vars) {
-    if (i < 0) {
-      i = 0;
+    if (*i < 0) {
+      *i = 0;
     }
 
-    if (i > 2) {
-      i = 2;
+    if (*i > 2) {
+      *i = 2;
     }
   }
 }
 
-std::string keypadTraverser::processInstructionSet(char* c) {
-  return "Hello";
-}
-
 int main() {
+  std::ifstream input("inputs/day2.txt");
+  if (!input) {
+    std::cerr << "Could not open input file\n";
+    return 1;
+  }
+
+  char c;
+  while (input.get(c)) {
+    processInstruction(c);
+  }
+
+  input.close();
+
+  std::cout << keycode;
   return 0;
 }
