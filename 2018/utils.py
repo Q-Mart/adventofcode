a79476fb503d4f23dc1a8b8a45b76e068dfe8459
Q-Mart <qumarthjash@gmail.com>
2018-12-03 19:57:56 +0000

    Day 4 2018

--- 2018/utils.py
+++ 2018/utils.py
@@ -1,6 +1,11 @@
+import re
+
 def getDay(dayNumber):
     lines = []
     with open("inputs/{0}.txt".format(dayNumber)) as f:
         lines = f.readlines()
         lines = list(map(str.strip, lines))
     return lines
+
+def getNumbers(string):
+    return re.findall(r'-?\d+').group()
