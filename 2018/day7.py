53db78132d4475912b72a11e760ce112a8b4885c
Q-Mart <qumarthjash@gmail.com>
2018-12-08 12:59:21 +0000

    Non working day7 part 2

--- 2018/day7.py
+++ 2018/day7.py
@@ -1,6 +1,47 @@
 import utils
 import re
 
+IDLE = 0
+WORKING = 1
+
+class Worker:
+    def __init__(self, ID, constant):
+        self.id = ID
+        self.constant = constant
+        self.state = IDLE
+        self.currentJob = None
+
+        self.completionTime = None
+        self.currentWorkTime = 1
+
+    def work(self, S, edges, completed):
+        if self.state == WORKING:
+
+            if self.currentWorkTime == self.completionTime:
+                self.completionTime = None
+                self.currentWorkTime = 1
+                self.state = IDLE
+                completed.add(self.currentJob)
+
+                for m in getChildren(self.currentJob, edges):
+                    del edges[edges.index((self.currentJob, m))]
+                    if noDependencies(m, edges):
+                        S.append(m)
+                self.currentJob = None
+
+            self.currentWorkTime += 1
+
+        if self.state == IDLE and S != []:
+            S = sorted(S, reverse=True)
+            n = S.pop()
+
+            self.currentJob = n
+            self.completionTime = ord(self.currentJob) - 64 + self.constant
+            self.state = WORKING
+            self.currentWorkTime = 1
+
+        return S, edges, completed
+
 def extractFirstLetter(text):
     return re.search(r'(?<=Step )\D', text).group()
 
@@ -12,6 +53,38 @@ def getChildren(n, edges):
 
 def noDependencies(m, edges):
     return list(filter(lambda e: m == e[1], edges)) == []
+
+def part1(S, edges):
+    L = []
+
+    while S != []:
+        S = sorted(S, reverse=True)
+        n = S.pop()
+        L.append(n)
+
+        for m in getChildren(n, edges):
+            del edges[edges.index((n, m))]
+            if noDependencies(m, edges):
+                S.append(m)
+
+    return ''.join(L)
+
+def part2(S, edges, numWorkers, constant, stepCharacters):
+    numSeconds = 0
+    completed = set()
+    workers = [Worker(i, constant) for i in range(numWorkers)]
+
+    print ('Seconds\t W1\t W2')
+    while completed != stepCharacters:
+        printString = '{0}\t'.format(numSeconds)
+        for w in workers:
+            S, edges, completed = w.work(S, edges, completed)
+            printString += '{0}\t'.format(w.currentJob)
+
+        numSeconds += 1
+        print(printString)
+
+    return numSeconds-1
 
 steps = utils.getDay(7)
 # steps = ['Step C must be finished before step A can begin.',
@@ -38,16 +111,13 @@ for c in stepCharacters:
     if noDependencies(c, edges):
         S.append(c)
 
-L = []
+print (part1(S, edges))
 
-while S != []:
-    S = sorted(S, reverse=True)
-    n = S.pop()
-    L.append(n)
+edges = []
+for s in steps:
+    first = extractFirstLetter(s)
+    second = extractSecondLetter(s)
+    edges.append((first,second))
 
-    for m in getChildren(n, edges):
-        del edges[edges.index((n, m))]
-        if noDependencies(m, edges):
-            S.append(m)
-
-print (''.join(L))
+# print (part2(S, edges, 2, 0, stepCharacters))
+print (part2(S, edges, 5, 60, stepCharacters))
