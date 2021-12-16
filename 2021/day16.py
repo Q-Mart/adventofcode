206909838f895d982ace345ded609483af1e1339
Q-Mart <qumarthjash@gmail.com>
2021-12-16 07:09:46 +0000

    D16P1. Broken Operator Parsing

--- 2021/day16.py
+++ 2021/day16.py
@@ -1,40 +1,23 @@
 import utils
 
-def to_binary(num: int) -> str:
-    return bin(num)
+def to_binary(num: str) -> str:
+    return bin(int(num,16))[2:]
 
 def to_decimal(string: str) -> int:
     return int(string, 2)
 
 class Packet:
-    def __init__(self, hex):
-        self._raw = to_binary(int(hex, 16))[2:]
-        self._version = to_decimal(self._raw[:3])
-        self._type = to_decimal(self._raw[3:6])
+    def __init__(self, version):
+        self._version = version
 
     @property
     def version(self):
         return self._version
 
-    @property
-    def type(self):
-        return self._type
-
 class Value(Packet):
-    def __init__(self, hex):
-        super().__init__(hex)
-
-        i = 6
-        value_str = ''
-        while i < len(self._raw) - 5:
-            extension = bool(int(self._raw[i]))
-            value_str += self._raw[i+1:i+5]
-
-            i += 5
-            if extension == False:
-                break
-
-        self._value = to_decimal(value_str)
+    def __init__(self, version, val):
+        super().__init__(version)
+        self._value = val
 
     @property
     def value(self):
@@ -43,7 +26,62 @@ class Value(Packet):
     def __str__(self):
         return str(self.value)
 
+class Operator(Packet):
+    def __init__(self, version, sub_packets):
+        super().__init__(version)
+        self._sub_packets = sub_packets
+
+def parse_val(raw):
+    i = 0
+    value_str = ''
+    while i < len(raw) - 5:
+        extension = bool(int(raw[i]))
+        value_str += raw[i+1:i+5]
+
+        i += 5
+        if extension == False:
+            break
+
+    return to_decimal(value_str)
+
+def parse_operator(raw):
+    ptr = 6
+    length_type = int(raw[ptr])
+    ptr += 1
+
+    length = None
+    if length_type == 0:
+        length = to_decimal(raw[ptr:ptr+15])
+        ptr += 15
+    else:
+        length = to_decimal(raw[ptr:ptr+11])
+        ptr += 11
+
+    sub_packets = parse(raw[ptr:ptr+length])
+    return sub_packets, ptr+length
+
+def parse(raw):
+    version = to_decimal(raw[:3])
+    type = to_decimal(raw[3:6])
+
+    print(version, type)
+    if type == 4:
+        val = parse_val(raw[6:])
+        return Value(version, val)
+
+    else:
+        packets = []
+        ptr = 0
+
+        while ptr <= len(raw):
+            sub_packets, ptr = parse_operator(raw[ptr:])
+            packets.append(Operator(version, sub_packets))
+
+        return packets
+
 data = utils.get_day(2021, 16)[0]
 
-test_1 = Value('D2FE28')
+test_1 = parse(to_binary('D2FE28'))
 assert test_1.value == 2021
+
+test_2 = parse(to_binary('38006F45291200'))
