111cac0f1803236b0f9e53a00aa92be6ab610aee
Q-Mart <qumarthjash@gmail.com>
2021-12-18 09:15:15 +0000

    D18P1: Implement splitting, still working on exploding

--- 2021/day18.py
+++ 2021/day18.py
@@ -1,4 +1,5 @@
 import utils
+import math
 
 class Node:
     def __init__(self, left, right):
@@ -20,9 +21,31 @@ class Node:
     def __repr__(self):
         return f'Node(l={self.left}, r={self.right})'
 
+def split(num):
+    return Node(math.floor(num/2), math.ceil(num/2))
+
+def traverse(tree, path=''):
+    if len(path) >= 4:
+        return tree, path
+
+    if type(tree.left) == list:
+        result = traverse(tree.left, path+'l')
+        if result != None:
+            return result
+
+    if type(tree.right) == list:
+        result = traverse(tree.right, path+'r')
+        if result != None:
+            return result
+
+    return None
+
 def find_first_node_at_depth(tree, depth=4, current_depth=0):
     if current_depth == depth:
         return tree
+    elif type(tree) == int:
+        # Can't go lower
+        return None
     else:
         return find_first_node_at_depth(tree.left, depth, current_depth+1)
 
@@ -34,5 +57,5 @@ def parse_to_tree(data):
 
 data = utils.get_day(2021, 18)
 
-t = parse_to_tree([[[[[9,8],1],2],3],4])
-print(find_first_node_at_depth(t))
+t = parse_to_tree([[6,[5,[4,[3,2]]]],1])
+print(traverse(t))
